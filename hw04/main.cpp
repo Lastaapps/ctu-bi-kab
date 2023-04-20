f8b576e86c20f8ae56d02cdcc44e3385f0138f5c
Petr Laštovička <lastope2@fit.cvut.cz>
2023-04-21 00:08:53 +0200

    hw04: Open added

--- hw04/main.cpp
+++ hw04/main.cpp
@@ -126,6 +126,12 @@ Option<size_t> readBytes(ifstream& in, size_t fileLen, uint8_t * bytes, size_t l
   return {(size_t) read};
 }
 
+Option<size_t> readBytesPrecise(ifstream& in, size_t fileLen, uint8_t * bytes, size_t len) {
+  Option<size_t> res = readBytes(in, fileLen, bytes, len);
+  if (!res.success) { return {}; }
+  if (*res == len) { return {*res}; } else { return {}; }
+}
+
 [[nodiscard]]
 Option<Unit> writeBytes(ofstream& out, const uint8_t * array, const size_t len) {
   out.write((char*) array, len);
@@ -174,7 +180,7 @@ struct FileRemoveTrap {
 // --- The Main work, oh no ---------------------------------------------------
 
 Option<Unit> initRandom() {
-  return boolToOpt(RAND_load_file("/dev/random", 32) != 32);
+  return boolToOpt(RAND_load_file("/dev/random", 32) == 32);
 }
 
 using PKeyPtr = std::unique_ptr<EVP_PKEY, decltype(&EVP_PKEY_free)>;
@@ -182,11 +188,33 @@ using CipherCtxPtr = std::unique_ptr<EVP_CIPHER_CTX, decltype(&EVP_CIPHER_CTX_fr
 
 
 Option<PKeyPtr> readPubKey(const char * filename) {
-  FILE * fp = fopen(filename, "br");
-  if (fp == nullptr) { return {}; };
+  FILE * fp = fopen(filename, "rb");
+  if (fp == nullptr) { OUT("Failed to open the file"); return {}; };
+
   auto ptr = PKeyPtr(PEM_read_PUBKEY(fp, NULL, NULL, NULL), &EVP_PKEY_free);
-  if (ptr == nullptr) { return {}; }
-  if (fclose(fp) != 0) { return {}; }
+  if (ptr == nullptr) {
+    OUT("Key is broken");
+    fclose(fp);
+    return {};
+  }
+
+  if (fclose(fp) != 0) { OUT("Failed to close the file"); return {}; }
+
+  return { std::move(ptr) };
+}
+
+Option<PKeyPtr> readPrivateKey(const char * filename) {
+  FILE * fp = fopen(filename, "rb");
+  if (fp == nullptr) { OUT("Failed to open the file"); return {}; };
+
+  auto ptr = PKeyPtr(PEM_read_PrivateKey(fp, NULL, NULL, NULL), &EVP_PKEY_free);
+  if (ptr == nullptr) {
+    OUT("Key is broken");
+    fclose(fp);
+    return {};
+  }
+
+  if (fclose(fp) != 0) { OUT("Failed to close the file"); return {}; }
   return { std::move(ptr) };
 }
 
@@ -199,40 +227,45 @@ bool seal(
   OpenSSL_add_all_ciphers();
   bindCheck(initRandom(), "Failed to init random");
 
+  // Open files
   auto trap = FileRemoveTrap(outFileName);
   bind(inFile,   openFileForReading(inFileName), "Failed open file for reading");
   bind(outFile,  openFileForWriting(outFileName), "Failed open file for writing");
   auto closer = FileAutoCloser(inFile, outFile);
   bind(fileSize, getFileSize(inFile), "Failed to get a file size");
 
-  bind(publicKey, readPubKey(publicKeyFileName), "Failed open perm for writing");
+  // Read key for the main key encryption
+  bind(key, readPubKey(publicKeyFileName), "Failed to load public key");
 
+  // Create a cipher
   const EVP_CIPHER* cipherType = EVP_get_cipherbyname(symmetricCipher);
   if (cipherType == nullptr) { return {}; }
   const int32_t nid = EVP_CIPHER_get_nid(cipherType);
 
+  // Create a cipher context
   auto cipherCtx = CipherCtxPtr(EVP_CIPHER_CTX_new(), &EVP_CIPHER_CTX_free);
   if (cipherCtx == nullptr) { return {}; }
-  if (EVP_CIPHER_CTX_init(cipherCtx.get())) { return {}; };
-  auto pkey = PKeyPtr(EVP_PKEY_new(), &EVP_PKEY_free);
-  if (pkey == nullptr) { return {}; }
+  if (!EVP_CIPHER_CTX_init(cipherCtx.get())) { return {}; };
 
+  // Load cipher info
   size_t ivTypeLen  = EVP_CIPHER_iv_length(cipherType);
-  size_t keyTypeLen = EVP_PKEY_size(pkey.get());
+  size_t keyTypeLen = EVP_PKEY_size(key.get());
   auto ivBuff = make_unique<uint8_t[]>(ivTypeLen);
   auto keyBuffRaw = new uint8_t[keyTypeLen];
   int keyLen;
 
-  auto publicKeyPtr = publicKey.get();
-  if (EVP_SealInit(cipherCtx.get(), cipherType, &keyBuffRaw, &keyLen, ivBuff.get(), &publicKeyPtr, 1)) { delete [] keyBuffRaw; return {}; }
+  // Init
+  auto publicKeyPtr = key.get();
+  if (!EVP_SealInit(cipherCtx.get(), cipherType, &keyBuffRaw, &keyLen, ivBuff.get(), &publicKeyPtr, 1)) { delete [] keyBuffRaw; return {}; }
   auto keyBuff = unique_ptr<uint8_t[]>(keyBuffRaw);
 
+  // Store cipher info
   bindCheck(writeBytes(outFile, (uint8_t*)&nid, sizeof(nid)), "Failed to write NID");
   bindCheck(writeBytes(outFile, (uint8_t*)&keyLen, sizeof(keyLen)), "Failed to write key len");
   bindCheck(writeBytes(outFile, keyBuff.get(), keyLen), "Failed to write key");
   bindCheck(writeBytes(outFile, ivBuff.get(), ivTypeLen), "Failed to write key");
 
-  
+  // Encrypt the file
   const size_t blockSize = (size_t) EVP_CIPHER_CTX_block_size(cipherCtx.get());
   const size_t chunkSize = 1024;
   const size_t outBuffSize = chunkSize + blockSize;
@@ -243,14 +276,14 @@ bool seal(
     bind(readCnt, readBytes(inFile, fileSize, inBuff.get(), chunkSize), ("Failed to read file"));
 
     int len = outBuffSize;
-    if (!EVP_CipherUpdate(cipherCtx.get(), outBuff.get(), &len, inBuff.get(), readCnt)) { OUT("Failed to update the context"); return false; }
+    if (!EVP_SealUpdate(cipherCtx.get(), outBuff.get(), &len, inBuff.get(), readCnt)) { OUT("Failed to update the context"); return false; }
 
     bindCheck(writeBytes(outFile, outBuff.get(), (size_t) len), "Failed to write the file")
 
     // File read, finalize
     if (readCnt != chunkSize) {
       int len = outBuffSize;
-      if (!EVP_CipherFinal(cipherCtx.get(), outBuff.get(), &len)) { OUT("Failed to finalize the context"); return false; }
+      if (!EVP_SealFinal(cipherCtx.get(), outBuff.get(), &len)) { OUT("Failed to finalize the context"); return false; }
 
       bindCheck(writeBytes(outFile, outBuff.get(), len), "Failed to write the file")
       break; 
@@ -258,16 +291,83 @@ bool seal(
   }
   
 
+  // Cleanup
   bindCheck(closer.close(), "Failed to close the files");
   trap.disable();
   return true;
 }
 
-bool open(const char * inFile,
-    const char * outFile,
-    const char * privateKeyFile
+bool open(const char * inFileName,
+    const char * outFileName,
+    const char * privateKeyFileName
     ) {
-  //waiting for code...
+  OpenSSL_add_all_ciphers();
+
+  // Open files
+  auto trap = FileRemoveTrap(outFileName);
+  bind(inFile,   openFileForReading(inFileName), "Failed open file for reading");
+  bind(outFile,  openFileForWriting(outFileName), "Failed open file for writing");
+  auto closer = FileAutoCloser(inFile, outFile);
+  bind(fileSize, getFileSize(inFile), "Failed to get a file size");
+
+  // Read a key for the main key decryption
+  bind(key, readPrivateKey(privateKeyFileName), "Failed to load perm");
+
+  // Read info from a file
+  int nid, keyLen;
+  bindCheck(readBytesPrecise(inFile, fileSize, (uint8_t*)&nid, sizeof(nid)), "Failed to read initial NID bytes");
+  bindCheck(readBytesPrecise(inFile, fileSize, (uint8_t*)&keyLen, sizeof(keyLen)), "Failed to read key length");
+
+  // Get cipher type
+  const EVP_CIPHER* cipherType = EVP_get_cipherbynid(nid);
+  if (cipherType == nullptr) { return {}; }
+
+  // Create context
+  auto cipherCtx = CipherCtxPtr(EVP_CIPHER_CTX_new(), &EVP_CIPHER_CTX_free);
+  if (cipherCtx == nullptr) { return {}; }
+  if (!EVP_CIPHER_CTX_init(cipherCtx.get())) { return {}; };
+
+  // Load the remaining data
+  size_t ivTypeLen  = EVP_CIPHER_iv_length(cipherType);
+  size_t keyTypeLen = EVP_PKEY_size(key.get());
+  auto ivBuff = make_unique<uint8_t[]>(ivTypeLen);
+  auto keyBuff = make_unique<uint8_t[]>(keyTypeLen);
+
+  bindCheck(readBytesPrecise(inFile, fileSize, keyBuff.get(), keyLen), "Failed to read key");
+  bindCheck(readBytesPrecise(inFile, fileSize, ivBuff.get(), ivTypeLen), "Failed to read iv");
+
+  // Init
+  if (!EVP_OpenInit(cipherCtx.get(), cipherType, keyBuff.get(), keyLen, ivBuff.get(), key.get())) { return {}; }
+
+  // Decrypt the file
+  const size_t blockSize = (size_t) EVP_CIPHER_CTX_block_size(cipherCtx.get());
+  const size_t chunkSize = 1024;
+  const size_t outBuffSize = chunkSize + blockSize;
+  auto  inBuff = make_unique<uint8_t[]>(chunkSize);
+  auto outBuff = make_unique<uint8_t[]>(outBuffSize);
+
+  while(true) {
+    // Encrypt the file
+    bind(readCnt, readBytes(inFile, fileSize, inBuff.get(), chunkSize), ("Failed to read file"));
+
+    int len = outBuffSize;
+    if (!EVP_OpenUpdate(cipherCtx.get(), outBuff.get(), &len, inBuff.get(), readCnt)) { OUT("Failed to update the context"); return false; }
+
+    bindCheck(writeBytes(outFile, outBuff.get(), (size_t) len), "Failed to write the file")
+
+    // File read, finalize
+    if (readCnt != chunkSize) {
+      int len = outBuffSize;
+      if (!EVP_OpenFinal(cipherCtx.get(), outBuff.get(), &len)) { OUT("Failed to finalize the context"); return false; }
+
+      bindCheck(writeBytes(outFile, outBuff.get(), len), "Failed to write the file")
+      break; 
+    }
+  }
+  
+  // Cleanup
+  bindCheck(closer.close(), "Failed to close the files");
+  trap.disable();
   return true;
 }
 
